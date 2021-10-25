2a6e2ab44e054479ce9ff5d733e973842d0f50ae
Your Name <you@example.com>
2021-10-25 14:05:17 +0300

    HW4

--- controllers/listContacts.js
+++ controllers/listContacts.js
@@ -1,7 +1,14 @@
 const { Contact } = require('../models')
 
 const listContacts = async (req, res) => {
-  const contacts = await Contact.find({}, '_id name email phone favorite')
+  const { _id } = req.user
+
+  const { page = 1, limit = 20 } = req.query
+  const skip = (page - 1) * limit
+
+  const contacts = await Contact.find({ owner: _id },
+    '_id name email phone favorite', { skip, limit: +limit }).populate('owner', 'email')
+
   res.json({
     status: 'success',
     code: 200,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,20 +2,20 @@ const express = require('express')
 const router = express.Router()
 
 const ctrl = require('../../controllers')
-const { controllerWrapper, validation } = require('../../middlewares')
+const { controllerWrapper, validation, authenticate } = require('../../middlewares')
 
 const { joiSchema } = require('../../models/contact')
 
-router.get('/', controllerWrapper(ctrl.listContacts))
+router.get('/', authenticate, controllerWrapper(ctrl.listContacts))
 
-router.get('/:id', controllerWrapper(ctrl.getContactById))
+router.get('/:id', authenticate, controllerWrapper(ctrl.getContactById))
 
-router.post('/', validation(joiSchema), controllerWrapper(ctrl.addContact))
+router.post('/', authenticate, validation(joiSchema), controllerWrapper(ctrl.addContact))
 
-router.delete('/:id', controllerWrapper(ctrl.removeContact))
+router.delete('/:id', authenticate, controllerWrapper(ctrl.removeContact))
 
-router.patch('/:id', validation(joiSchema), controllerWrapper(ctrl.updateContactById))
+router.patch('/:id', authenticate, validation(joiSchema), controllerWrapper(ctrl.updateContactById))
 
-router.patch('/:id/favorite', controllerWrapper(ctrl.updateStatusContact))
+router.patch('/:id/favorite', authenticate, controllerWrapper(ctrl.updateStatusContact))
 
 module.exports = router
